9672300fa0ffbf99ab8c16b7963a9f9691a26dda
ericdavelaurel <143635115+ericdavelaurel@users.noreply.github.com>
2024-02-08 14:46:34 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -47,7 +47,7 @@ export const NotionPageHeader: React.FC<{
       <div className='notion-nav-header'>
         <Breadcrumbs block={block} rootOnly={true} />
 
-        <div className='notion-nav-header-rhs breadcrumbs'>
+{/*         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
             ?.map((link, index) => {
               if (!link.pageId && !link.url) {
@@ -81,7 +81,7 @@ export const NotionPageHeader: React.FC<{
           <ToggleThemeButton />
 
           {isSearchEnabled && <Search block={block} title={null} />}
-        </div>
+        </div> */}
       </div>
     </header>
   )
