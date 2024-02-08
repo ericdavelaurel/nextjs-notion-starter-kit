733b980a92a96243773b7b24433ddfd3ed724ddb
ericdavelaurel <143635115+ericdavelaurel@users.noreply.github.com>
2024-02-08 14:49:39 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -33,56 +33,56 @@ const ToggleThemeButton = () => {
   )
 }
 
-export const NotionPageHeader: React.FC<{
-  block: types.CollectionViewPageBlock | types.PageBlock
-}> = ({ block }) => {
-  const { components, mapPageUrl } = useNotionContext()
+// export const NotionPageHeader: React.FC<{
+//   block: types.CollectionViewPageBlock | types.PageBlock
+// }> = ({ block }) => {
+//   const { components, mapPageUrl } = useNotionContext()
 
-  if (navigationStyle === 'default') {
-    return <Header block={block} />
-  }
+//   if (navigationStyle === 'default') {
+//     return <Header block={block} />
+//   }
 
-  // return (
-  //   <header className='notion-header'>
-  //     <div className='notion-nav-header'>
-  //       <Breadcrumbs block={block} rootOnly={true} />
+//   return (
+//     <header className='notion-header'>
+//       <div className='notion-nav-header'>
+//         <Breadcrumbs block={block} rootOnly={true} />
 
-  //       <div className='notion-nav-header-rhs breadcrumbs'>
-  //         {navigationLinks
-  //           ?.map((link, index) => {
-  //             if (!link.pageId && !link.url) {
-  //               return null
-  //             }
+//         <div className='notion-nav-header-rhs breadcrumbs'>
+//           {navigationLinks
+//             ?.map((link, index) => {
+//               if (!link.pageId && !link.url) {
+//                 return null
+//               }
 
-  //             if (link.pageId) {
-  //               return (
-  //                 <components.PageLink
-  //                   href={mapPageUrl(link.pageId)}
-  //                   key={index}
-  //                   className={cs(styles.navLink, 'breadcrumb', 'button')}
-  //                 >
-  //                   {link.title}
-  //                 </components.PageLink>
-  //               )
-  //             } else {
-  //               return (
-  //                 <components.Link
-  //                   href={link.url}
-  //                   key={index}
-  //                   className={cs(styles.navLink, 'breadcrumb', 'button')}
-  //                 >
-  //                   {link.title}
-  //                 </components.Link>
-  //               )
-  //             }
-  //           })
-  //           .filter(Boolean)}
+//               if (link.pageId) {
+//                 return (
+//                   <components.PageLink
+//                     href={mapPageUrl(link.pageId)}
+//                     key={index}
+//                     className={cs(styles.navLink, 'breadcrumb', 'button')}
+//                   >
+//                     {link.title}
+//                   </components.PageLink>
+//                 )
+//               } else {
+//                 return (
+//                   <components.Link
+//                     href={link.url}
+//                     key={index}
+//                     className={cs(styles.navLink, 'breadcrumb', 'button')}
+//                   >
+//                     {link.title}
+//                   </components.Link>
+//                 )
+//               }
+//             })
+//             .filter(Boolean)}
 
-  //         <ToggleThemeButton />
+//           <ToggleThemeButton />
 
-  //         {isSearchEnabled && <Search block={block} title={null} />}
-  //       </div>
-      </div>
-    </header>
-  )
+//           {isSearchEnabled && <Search block={block} title={null} />}
+//         </div>
+//       </div>
+//     </header>
+//   )
 }
